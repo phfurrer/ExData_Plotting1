94b8493006c48de26661fac48827c9b3d6c56ce5
phfurrer <ph.furrer@fastmail.fm>
2015-11-08 16:47:29 +0100

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,15 @@
+# Import and prepare data
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+data_all <- read.csv("household_power_consumption.txt", header=TRUE, sep=';', na.strings="?", nrows=2075259, quote='\"')
+data_sub <- data_all[data_all$Date %in% c("1/2/2007","2/2/2007"),]
+
+datetime <- strptime(paste(data_sub$Date, data_sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(data_sub$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
